6369e3dc5c11f09088d6e8620896833f3ad2d8dc
Arian Sani <ariansani@hotmail.com>
2022-03-23 18:24:29 +0800

    Added

---
+++ src/test/java/VTTP2022/NUSISS/MarchBinanceAPI/ApplicationTests.java
@@ -0,0 +1,13 @@
+package VTTP2022.NUSISS.MarchBinanceAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
